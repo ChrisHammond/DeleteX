96ef821bccc39ac812ccb48ba5d9b0c406be9f7e
Chris Hammond <christoc@live.com>
2023-09-20 10:00:39 -0500

    Updates to repo with actual code

---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿using System.Diagnostics;
+using LinqToTwitter;
+using LinqToTwitter.OAuth;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+//need to install the following via command line
+// dotnet add package LinqToTwitter
+
+class DeleteX
+{
+    //create your application via developers.twitter.com
+    //Under the oauth 2.0 section be sure to request read/write access
+    //generate the auth tokens.
+    private static string twitterApiKey = ""; // Here you need to put your Twitter Developer API Key
+    private static string twitterApiKeySecret = ""; // Here you need to put your Twitter Developer API Key Secret
+    private static string accessToken = ""; // Access Token 
+    private static string accessTokenSecret = ""; // Access Token
+
+    //No need to change these paths unless you put files somewhere else.
+    private static readonly string deletedTweetsPath = Path.Combine(Directory.GetCurrentDirectory(), "deletedTweets.txt");
+    private static readonly string tweetsPath = Path.Combine(Directory.GetCurrentDirectory(), "tweets.csv");
+
+    private static string[] tweetIds;
+    private static int currentIndex = 0;
+
+    private static TwitterContext twitterCtx;
+
+    static void Main()
+    {
+        if (!InitializeTwitterContextFromSavedCredentials())
+        {
+            Console.WriteLine("Could not authenticate with saved credentials. Did you forget to populate them? \n\n\nGo back and edit Program.cs to populate them.");
+            return;            
+        }
+
+        // Load tweet IDs from the file
+        tweetIds = File.ReadAllLines(tweetsPath);
+
+        // Load deleted tweet list
+        var deletedTweets = new HashSet<string>(File.ReadAllLines(deletedTweetsPath));
+
+        // Remove already deleted tweets from our list, no need to attempt to delete them again
+        tweetIds = tweetIds.Where(id => !deletedTweets.Contains(id)).ToArray();
+
+        // Set up a timer to delete tweets every 15 seconds
+        Timer timer = new Timer(15000);
+        timer.Elapsed += async (sender, e) => await DeleteTweet(sender, e);
+        timer.Start();
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+
+    static bool InitializeTwitterContextFromSavedCredentials()
+    {
+
+        if (twitterApiKey == string.Empty || twitterApiKeySecret == string.Empty || accessToken == string.Empty || accessTokenSecret == string.Empty)
+            return false;
+
+        var authorizer = new SingleUserAuthorizer
+        {
+            CredentialStore = new SingleUserInMemoryCredentialStore
+            {
+                ConsumerKey = twitterApiKey, 
+                ConsumerSecret = twitterApiKeySecret, 
+                AccessToken = accessToken,
+                AccessTokenSecret = accessTokenSecret
+            }
+        };
+
+        twitterCtx = new TwitterContext(authorizer);
+        return true;
+    }
+
+    static async Task DeleteTweet(object sender, ElapsedEventArgs e)
+    {
+        if (currentIndex >= tweetIds.Length)
+        {
+            Console.WriteLine("All tweets processed! Press any key to stop the program.");
+            return;
+        }
+
+        var tweetId = tweetIds[currentIndex];
+
+        // Call the Twitter API to delete the tweet
+        bool success = await DeleteTweetViaAPI(tweetId);
+
+        if (success)
+        {
+            // Log the deleted tweet ID
+            File.AppendAllText(deletedTweetsPath, tweetId + Environment.NewLine);
+            Console.WriteLine($"Deleted tweet with ID {tweetId}");
+        }
+        else
+        {
+            Console.WriteLine($"Failed to delete tweet with ID {tweetId}");
+            //if you want to go ahead and log this one as deleted, uncomment the next line. Might make sense to throw this into a separate file
+            //File.AppendAllText(deletedTweetsPath, tweetId + Environment.NewLine);
+        }
+
+        currentIndex++;
+    }
+
+    static async Task<bool> DeleteTweetViaAPI(string tweetId)
+    {
+        try
+        {
+            if (twitterCtx != null)
+            { 
+                var tweet = await twitterCtx.DeleteTweetAsync(tweetId);
+
+                if (tweet != null)
+                {
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error deleting tweet: {ex.Message}");
+            return false;
+        }
+    }
+
+}
---
+++ exporttweets.ps1
@@ -0,0 +1,24 @@
+# This script is used to take the Tweets.js file from your Twitter archive and convert it to a CSV with a list of all TweetIds on separate lines. This then gets used by the DeleteX tool to delete all your tweets.
+# Paths
+$tweetJsPath = "tweets.js"
+$outputCsvPath = "tweets.csv"
+
+# Read the cleaned tweet.js file content
+$content = Get-Content -Path $tweetJsPath -Raw
+
+# Convert the content to a PowerShell object
+try {
+    $tweets = $content | ConvertFrom-Json
+
+    # Extract the id_str values, sort them in ascending order
+    $sortedTweets = $tweets | Sort-Object { [int64] $_.tweet.id_str }
+
+    # Write the sorted tweet IDs to the file
+    "TweetID" | Out-File $outputCsvPath -Encoding utf8
+    $sortedTweets | ForEach-Object { $_.tweet.id_str } | Out-File $outputCsvPath -Append -Encoding utf8
+
+    Write-Host "Finished writing to $outputCsvPath"
+}
+catch {
+    Write-Host "Error converting JSON: $_"
+}
